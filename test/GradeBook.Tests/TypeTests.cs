fbaef9d378a5d2973b30d6291596dba36db8a0b3
JhonnyLeandroMeloE <ejohhnny@gmail.com>
2021-05-19 19:45:05 -0500

    Add test cases for reference types and value types

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,80 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
+        [Fact]
+        public void StringBehavesLikeValueTypes()
+        {
+            string name = "Scott";
+            string upper = MakeUpperCase(name);
+
+            Assert.Equal("Scott", name);
+            Assert.Equal("SCOTT", upper);
+        }
+
+        private string MakeUpperCase(string name)
+        {
+            return name.ToUpper();
+        }
+
+        [Fact]
+        public void test2()
+        {
+            var x = GetInt();
+            SetInt(out x);
+            Assert.Equal(42,x);
+        }
+        
+
+        private void SetInt( out int x){
+            x = 42;
+        }
+
+        private int GetInt(){
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCAnPassByREf()
+        {
+            var book = GetBook("Book 1");
+            GetBookSetName(ref book, "New name");
+
+            Assert.Equal("New name", book.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name){
+            book = new Book(name);
+        }
+
+
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book = GetBook("Book 1");
+            GetBookSetName(book, "New name");
+
+            Assert.Equal("Book 1", book.Name);
+        }
+
+        private void GetBookSetName(Book book, string name){
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void test1()
+        {
+            var book = GetBook("Book 1");
+            SetName(book, "New name");
+
+            Assert.Equal("New name", book.Name);
+        }
+
+        private void SetName(Book book, string name){
+            book.Name = name;
+        }
+
         [Fact]
         public void GetBookReturnsDifferentObjcets()
         {
